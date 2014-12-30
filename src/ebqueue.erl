db83e36c0fa4ec3cced6a720f4ffe720ce3325bc
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-12-30 02:10:47 -0500

    initial implementation

--- src/ebqueue.erl
+++ src/ebqueue.erl
@@ -1,9 +1,63 @@
 -module(ebqueue).
 
--export ([make/0, pop/1, push/2]).
+-behaviour(gen_server).
 
-make() -> {[], []}.
+%% API functions
+-export([start_link/0, in/2, out/1]).
 
-pop(_) -> empty.
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
 
-push(q, e) -> {[q], e}.
+-record(state, {
+          waiters :: queue:queue(_),
+          elements :: queue:queue(_)
+         }).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+in(Element, Pid) -> gen_server:cast(Pid, {enqueue, Element}).
+
+out(Pid) -> gen_server:cast(Pid, {dequeue, self()}).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+init([]) ->
+    {ok, #state{waiters=queue:new(), elements=queue:new()}}.
+
+handle_call(dequeue, From, State) ->
+    case queue:out(State#state.elements) of
+        {empty, _} ->
+            NewW = queue:in(State#state.waiters, From),
+            {noreply, State#state{waiters=NewW}};
+        {{value, E}, NewQ} ->
+            {reply, E, State#state{elements=NewQ}}
+    end.
+
+handle_cast({enqueue, Element}, State) ->
+    case queue:out(State#state.waiters) of
+        {empty, _} -> %% no waiters we just save the item
+            Q = State#state.elements,
+            NewQ = queue:in(Element, Q),
+            {noreply, State#state{elements=NewQ}};
+        {{value, W}, NewW} -> %% feed next waiter
+            gen_server:reply(W, {ok, Element}),
+            {noreply, State#state{waiters=NewW}}
+    end.
+
+handle_info(_Info, State) -> {noreply, State}.
+
+terminate(_Reason, _State) -> ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
